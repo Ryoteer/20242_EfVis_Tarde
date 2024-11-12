fcc5a9c07443aabde0618dfb63c70615952fc7f5
Ryoteer <72623439+Ryoteer@users.noreply.github.com>
2024-11-12 15:50:49 -0300

    Clase 14
    Efectos visuales complejos

---
+++ Assets/Scripts/Props/SwordSwitch.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class SwordSwitch : MonoBehaviour
+{
+    [Header("<color=orange>Renderer</color>")]
+    [SerializeField] private float _transitionTime = 0.5f;
+
+    private Animator _animator;
+    private Material _swordMat;
+    private VisualEffect _swordVfx;
+
+    private bool _enabledState = false, _isChanging = false;
+    private Vector4 _actualColor, _randomColor;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _swordMat = GetComponentInChildren<Renderer>().material;
+        _swordVfx = GetComponentInChildren<VisualEffect>();
+
+        _actualColor = _swordMat.GetColor("_AuraColor");
+        _swordVfx.SetVector4("GeneralColor", _actualColor);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _enabledState = !_enabledState;
+
+            _animator.SetBool("isEnabled", _enabledState);
+            _animator.SetTrigger("onSwitch");
+        }
+        else if (Input.GetKeyDown(KeyCode.C) && _enabledState && !_isChanging)
+        {
+            StartCoroutine(ChangeColor());
+        }
+    }
+
+    private IEnumerator ChangeColor()
+    {
+        _isChanging = true;
+
+        _randomColor = new Color(Random.Range(0.5f, 1.25f), Random.Range(0.5f, 1.25f), Random.Range(0.5f, 1.25f), 1.0f);
+
+        float t = 0.0f;
+
+        while (t < 1)
+        {
+            t += Time.deltaTime / _transitionTime;
+
+            _swordMat.SetColor("_AuraColor", Vector4.Lerp(_actualColor, _randomColor, t));
+            _swordVfx.SetVector4("GeneralColor", Vector4.Lerp(_actualColor, _randomColor, t));
+
+            yield return null;
+        }
+
+        _actualColor = _randomColor;
+
+        _isChanging = false;
+    }
+
+}
